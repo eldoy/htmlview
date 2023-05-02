a01be8b4422a4da1e415bf61500694ad2c361444
Vidar Eldøy <vidar@eldoy.com>
2023-05-02 09:16:03 +0200

    Simplify structure

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const h = require('./lib/h.js')
+const { esc } = require('haka')
 
-module.exports = { h }
+module.exports = function h(strings, ...keys) {
+  var s = ''
+  for (let i = 0; i < strings.length; i++) {
+    var text = strings[i]
+    var val = keys[i]
+
+    if (typeof val == 'function') {
+      val = val()
+    } else if (Array.isArray(val)) {
+      val = val.join('')
+    } else if (typeof val == 'string') {
+      if (text.slice(-1) == '$') {
+        text = text.slice(0, -1)
+      } else if (typeof esc == 'function') {
+        val = esc(val)
+      }
+    }
+    s += `${text}${val || ''}`
+  }
+  return s
+}
--- scripts/build.js
+++ scripts/build.js
@@ -3,15 +3,13 @@ const fs = require('fs')
 const uglify = require('uglify-js')
 const htmlview = require('../index.js')
 
-const code = []
-for (const fn in htmlview) {
-  code.push(`window.${fn} = ${htmlview[fn].toString()}\n`)
-}
+const code = `window.h = ${htmlview.toString()}\n`
+
 if (!fs.existsSync('./dist')) {
   fs.mkdirSync('./dist')
 }
 
-fs.writeFileSync('./dist/htmlview.js', code.join('\n'))
+fs.writeFileSync('./dist/htmlview.js', code)
 
-const result = uglify.minify(code.join('\n'))
+const result = uglify.minify(code)
 fs.writeFileSync('./dist/htmlview-min.js', result.code)
